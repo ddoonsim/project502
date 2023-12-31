7c52dcfd8c28521ec2174fc738eadaef60c5341f
ddoonsim <2sujg97@naver.com>
2023-12-31 12:36:50 +0900

    [회원가입] 회원 엔티티 생성

---
+++ src/main/java/org/choongang/entities/Member.java
@@ -0,0 +1,14 @@
+package org.choongang.entities;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+@Entity
+public class Member extends Base {
+}
