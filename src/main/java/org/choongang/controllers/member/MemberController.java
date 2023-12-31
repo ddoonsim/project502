e4c87f096c06d102bbf5768022849a0e4536ee02
ddoonsim <2sujg97@naver.com>
2023-12-31 11:04:49 +0900

    device 모드 수동 전환 혹은 요청 헤더의 User-Agent 정보를 가지고 모바일인지 체크

--- src/main/java/org/choongang/controllers/member/MemberController.java
+++ src/main/java/org/choongang/controllers/member/MemberController.java
@@ -1,18 +1,25 @@
 package org.choongang.controllers.member;
 
+import lombok.RequiredArgsConstructor;
+import org.choongang.commons.Utils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final Utils utils ;    // 편의 기능
 
     /**
      * 회원가입
      */
     @GetMapping("/join")
     public String join() {
-        return "front/member/join" ;
+
+        // device가 모바일인지 PC인지에 따라 view 경로 분리
+        return utils.tpl("member/join") ;
     }
 }
