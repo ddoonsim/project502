46419da23116b249435b1ce6bb6d21ff1b5982a5
ddoonsim <2sujg97@naver.com>
2024-01-02 14:06:20 +0900

    회원가입 DB 처리 오류 수정

--- src/main/java/org/choongang/member/service/JoinService.java
+++ src/main/java/org/choongang/member/service/JoinService.java
@@ -5,7 +5,6 @@ import org.choongang.member.controllers.JoinValidator;
 import org.choongang.member.controllers.RequestJoin;
 import org.choongang.member.entities.Member;
 import org.choongang.member.repositories.MemberRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
@@ -27,8 +26,12 @@ public class JoinService {
         // 비번 BCrypt로 해시화
         String hash = encoder.encode(form.getPassword()) ;
 
-        Member member = new ModelMapper().map(form, Member.class);    // 자동 setter
+//        Member member = new ModelMapper().map(form, Member.class);    // 자동 setter
+        Member member = new Member() ;
+        member.setEmail(form.getEmail());
+        member.setUserId(form.getUserId());
         member.setPassword(hash);
+        member.setName(form.getName());
 
         process(member);
     }
