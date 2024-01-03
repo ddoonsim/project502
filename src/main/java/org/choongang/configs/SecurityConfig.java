4fa21bd59a0a40e60853f64aa82d8098f4292a07
ddoonsim <2sujg97@naver.com>
2024-01-03 11:36:18 +0900

    권한 구분 부여, 타임리프와 연결

--- src/main/java/org/choongang/configs/SecurityConfig.java
+++ src/main/java/org/choongang/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.choongang.configs;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.choongang.member.service.LoginFailureHandler;
 import org.choongang.member.service.LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
@@ -33,6 +34,27 @@ public class SecurityConfig {
                     .logoutSuccessUrl("/member/login") ;    // 로그아웃 시에 이동할 페이지
         });
         /* 인증 설정 E - 로그인, 로그아웃 */
+
+        /* 인가 설정 S - 접근 통제 */
+        http.authorizeHttpRequests(c -> {
+            c.requestMatchers("/mypage/**").authenticated()    // mypage를 포함한 하위 페이지를 회원 전용 페이지로 설정
+                    .requestMatchers("/admin/**").hasAnyAuthority("ADMIN", "MANAGER")
+                        // admin을 포함한 하위 페이지를 관리자 전용 페이지로
+                    .anyRequest().permitAll() ;    // 그 외 모든 페이지는 모든 권한으로 접근 가능
+        });
+
+        http.exceptionHandling(c -> {
+            // 권한이 허용하지 않은 페이지에 접근한 경우에 띄울 view 페이지를 상세하게 설정
+            c.authenticationEntryPoint((req, res, e) -> {
+                String URL = req.getRequestURI() ;
+                if (URL.indexOf("/admin") != -1) {    // 관리자 페이지 ==> 403 응답 코드
+                    res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                } else {    // 회원 전용 페이지 ==> 로그인 페이지로
+                    res.sendRedirect(req.getContextPath() + "/member/login");
+                }
+            });
+        });
+        /* 인가 설정 E - 접근 통제 */
 
         // 시큐리티 설정 무력화
         return http.build() ;
--- src/test/java/org/choongang/ProjectApplicationTests.java
+++ src/test/java/org/choongang/ProjectApplicationTests.java
@@ -1,15 +1,34 @@
 package org.choongang;
 
+import org.choongang.member.Authority;
+import org.choongang.member.entities.Authorities;
+import org.choongang.member.entities.Member;
+import org.choongang.member.repositories.AuthoritiesRepository;
+import org.choongang.member.repositories.MemberRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = "spring.profiles.active=test")
 class ProjectApplicationTests {
 
-	@Test
+	@Autowired
+	private MemberRepository memberRepository ;
+	@Autowired
+	private AuthoritiesRepository authoritiesRepository ;
+
+	@Test @Disabled  // 실행 X
 	void contextLoads() {
+		// 회원 조회
+		Member member = memberRepository.findByUserId("user01").orElse(null) ;
+
+		// ADMIN 권한 부여
+		Authorities authorities = new Authorities() ;
+		authorities.setMember(member);
+		authorities.setAuthority(Authority.ADMIN);
+
+		authoritiesRepository.saveAndFlush(authorities) ;    // UPDATE 쿼리 실행
 	}
 
 }
