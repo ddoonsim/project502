405129504b9734fa4419de6c785e6d13b2852369
ddoonsim <2sujg97@naver.com>
2024-01-03 09:16:22 +0900

    로그아웃 처리 기능 구현

--- src/main/java/org/choongang/configs/SecurityConfig.java
+++ src/main/java/org/choongang/configs/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * 시큐리티 설정 클래스
@@ -18,7 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        /* 인증 설정 S - 로그인 */
+        /* 인증 설정 S - 로그인, 로그아웃 */
         http.formLogin(f -> {
             f.loginPage("/member/login")
                     .usernameParameter("username")
@@ -26,7 +27,12 @@ public class SecurityConfig {
                     .successHandler(new LoginSuccessHandler())    // 로그인 성공 시의 상세한 설정
                     .failureHandler(new LoginFailureHandler()) ;  // 로그인 실패 시의 상세한 설정
         });
-        /* 인증 설정 E - 로그인 */
+
+        http.logout(c -> {
+            c.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                    .logoutSuccessUrl("/member/login") ;    // 로그아웃 시에 이동할 페이지
+        });
+        /* 인증 설정 E - 로그인, 로그아웃 */
 
         // 시큐리티 설정 무력화
         return http.build() ;
