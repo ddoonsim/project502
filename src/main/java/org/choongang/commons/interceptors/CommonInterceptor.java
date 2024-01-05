78049d3690de563843d6f245e32d41649b6ad459
ddoonsim <2sujg97@naver.com>
2024-01-05 09:26:15 +0900

    오류페이지가 뜨지 않는 오류 해결

--- src/main/java/org/choongang/commons/interceptors/CommonInterceptor.java
+++ src/main/java/org/choongang/commons/interceptors/CommonInterceptor.java
@@ -3,22 +3,31 @@ package org.choongang.commons.interceptors;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.choongang.admin.config.controllers.BasicConfig;
+import org.choongang.admin.config.service.ConfigInfoService;
 import org.choongang.member.MemberUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.Arrays;
+
 /**
  * 공통 인터셉터
  */
 @Component
+@RequiredArgsConstructor
 public class CommonInterceptor implements HandlerInterceptor {
+
+    private final ConfigInfoService infoService ;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         checkDevice(request);    // 함수로 기능 분리
         clearLoginData(request) ;    // 로그인 경고 메세지가 저장되어 있는 세션 비우기
+        loadSiteConfig(request) ;
 
         return true ;
     }
@@ -49,5 +58,24 @@ public class CommonInterceptor implements HandlerInterceptor {
             HttpSession session = request.getSession();
             MemberUtil.clearLoginData(session);
         }
+    }
+
+    /**
+     * admin의 기본설정을 페이지마다 적용하는 메서드
+     *     ## controller 뿐만 아니라 모든 css, js파일 로드할 때마다 쿼리 실행하는 문제 발생
+     */
+    private void loadSiteConfig(HttpServletRequest request) {
+        // 로드할 때 쿼리 실행에서 제외할 파일들의 확장자
+        String[] excludes = {".js", ".css", ".png", ".jpg", ".jpeg", "gif", ".pdf", ".xls", ".xlxs", ".ppt"} ;
+        String URL = request.getRequestURI().toLowerCase() ;
+        boolean isIncluded = Arrays.stream(excludes).anyMatch(s -> URL.contains(s)) ;    // URL문자열 내에 excludes 중에서 일치하는 문자열이 있으면 true 반환
+        if (isIncluded) {
+            // true 이면 쿼리 실행 X하고 메서드 종료
+            return;
+        }
+
+        BasicConfig config = infoService.get("basic", BasicConfig.class).orElseGet(BasicConfig::new) ;
+
+        request.setAttribute("siteConfig", config);
     }
 }
