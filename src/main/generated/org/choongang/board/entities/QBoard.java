bf69a44100222d2c6fc25bdb66ebe9ed198319b3
ddoonsim <2sujg97@naver.com>
2024-01-10 16:01:20 +0900

    관리자 페이지의 게시판 관리 메뉴의 게시판 등록 --> 게시판 엔터티(미완)

---
+++ src/main/generated/org/choongang/board/entities/QBoard.java
@@ -0,0 +1,35 @@
+package org.choongang.board.entities;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QBoard is a Querydsl query type for Board
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QBoard extends EntityPathBase<Board> {
+
+    private static final long serialVersionUID = 1615681981L;
+
+    public static final QBoard board = new QBoard("board");
+
+    public QBoard(String variable) {
+        super(Board.class, forVariable(variable));
+    }
+
+    public QBoard(Path<? extends Board> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBoard(PathMetadata metadata) {
+        super(Board.class, metadata);
+    }
+
+}
+
